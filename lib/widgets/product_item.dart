1fbecdc3a1dbde246f3c3cfbd92dfa395797dcdd
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-24 16:58:57 +0200

    using nested models and providers

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,40 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/product.dart';
+
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  ProductItem(this.id, this.title, this.imageUrl);
+  // ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
             Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName, arguments: id,
+              ProductDetailScreen.routeName,
+              arguments: product.id,
             );
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
           leading: IconButton(
             icon: Icon(
-              Icons.favorite,
+              product.isFavourite ? Icons.favorite : Icons.favorite_border,
             ),
-            onPressed: () {},
+            onPressed: () {
+              product.toggleFavouriteStatus();
+            },
             color: Theme.of(context).accentColor,
           ),
           backgroundColor: Colors.black87,
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(

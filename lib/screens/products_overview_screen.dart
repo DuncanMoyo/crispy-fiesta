726ce6d04777b9a8504b31e69f83b66cc73d3076
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-24 19:17:54 +0200

    local state vs App-wide State

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
 import '../widgets/products_grid.dart';
+// import '../providers/products.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+// import 'package:provider/provider.dart';
 
+enum FilterOptions {
+  Favourites,
+  All,
+}
 
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavourites = false;
   @override
   Widget build(BuildContext context) {
+    // final productsContainer = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favourites'),
+                value: FilterOptions.Favourites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+            icon: Icon(Icons.more_vert),
+            onSelected: (FilterOptions selectedValue) {
+              // print(selectedValue);
+              setState(
+                () {
+                  if (selectedValue == FilterOptions.Favourites) {
+                    _showOnlyFavourites = true;
+                  } else {
+                    _showOnlyFavourites = false;
+                  }
+                },
+              );
+            },
+          ),
+        ],
       ),
-      body: ProductsGrid(
-        
-      ),
+      body: ProductsGrid(_showOnlyFavourites),
     );
   }
 }
-

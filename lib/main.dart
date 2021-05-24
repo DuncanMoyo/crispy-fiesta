7b17d6359aba4eecbe3b17160019ff94d5edfcf7
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-24 14:19:12 +0200

    working with Providers and Listeners

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:provider/provider.dart';
+
 import 'package:flutter/material.dart';
 import 'screens/product_detail_screen.dart';
 import 'screens/products_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,19 +12,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Duncan\'s Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
-      ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()
-      }
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+          title: 'Duncan\'s Shop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()
+          }),
     );
   }
 }
-
-
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -16,3 +16,4 @@ class ProductDetailScreen extends StatelessWidget {
     );
   }
 }
+ 

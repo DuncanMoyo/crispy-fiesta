3782047610adb7c10c5da94c901a0e1766d2a78c
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-26 12:25:52 +0200

    Making cart items dismissible

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
+
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
   CartItem(
     this.id,
+    this.productId,
     this.price,
     this.quantity,
     this.title,
@@ -15,25 +20,48 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 14,
-        vertical: 4,
+    return Dismissible(
+      key: ValueKey(id),
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId,);
+      },
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(Icons.delete, color: Colors.white, size: 40,),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+         margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: FittedBox(
-                child: Text('R $price'),
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: FittedBox(
+                  child: Text(
+                    'R $price',
+                  ),
+                ),
               ),
             ),
+            title: Text(title),
+            subtitle: Text(
+              'Total: R ${(price * quantity)}',
+            ),
+            trailing: Text(
+              '$quantity x',
+            ),
           ),
-          title: Text(title),
-          subtitle: Text('Total: R ${(price * quantity)}'),
-          trailing: Text('$quantity x'),
         ),
       ),
     );

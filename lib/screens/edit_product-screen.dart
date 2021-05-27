0a6da97cd72bb09ef48715b622ea310e5a7545a0
Duncan Moyo <duncanfmoyo@gmail.com>
2021-05-27 16:02:01 +0200

    saving new products

--- lib/screens/edit_product-screen.dart
+++ lib/screens/edit_product-screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../providers/product.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -57,10 +59,13 @@ class _EditProductScreenState extends State<EditProductScreen> {
       return;
     }
     _form.currentState.save();
-    print(_editedProduct.title);
-    print(_editedProduct.price);
-    print(_editedProduct.description);
-    print(_editedProduct.imageUrl);
+    Provider.of<Products>(
+      context,
+      listen: false,
+    ).addProduct(
+      _editedProduct,
+    );
+    Navigator.of(context).pop();
   }
 
   @override

7c20719fce71a0baeaace96fe0749d8583fa8741
Duncan Moyo <duncanfmoyo@gmail.com>
2021-06-02 08:43:17 +0200

    fetching orders and fixing an issue

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -25,6 +25,40 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    var url = Uri.https(
+      'flutter-shop-aaf22-default-rtdb.firebaseio.com',
+      '/orders.json',
+    );
+    final response = await http.get(url);
+    // print(json.decode(response.body));
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if(extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          dateTime: DateTime.parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                    id: item['id'],
+                    price: item['price'],
+                    quantity: item['quantity'],
+                    title: item['title']),
+              )
+              .toList(),
+        ),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     var url = Uri.https(
       'flutter-shop-aaf22-default-rtdb.firebaseio.com',

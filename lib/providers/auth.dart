80205de14e9d61cad6a232b2d4fff74c376f63ea
Duncan Moyo <duncanfmoyo@gmail.com>
2021-06-06 13:45:39 +0200

    allowing users to log in

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,10 +8,10 @@ class Auth with ChangeNotifier {
   String _userId;
   bool returnSecureToken;
 
-  Future<void> signup(String email, String password) async {
-    var url = Uri.https(
+  Future<void> _authenticate(String email, String password, String urlSegment) async {
+     final url = Uri.https(
       'identitytoolkit.googleapis.com',
-      '/v1/accounts:signUp',
+      '/v1/accounts:$urlSegment',
       {'key': 'AIzaSyDGmIK7jQi11qfdn3lE3_qlBHu42wTGWMw'},
     );
     final response = await http.post(
@@ -23,5 +23,13 @@ class Auth with ChangeNotifier {
       }),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
